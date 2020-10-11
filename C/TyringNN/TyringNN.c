3fdb561e41a67accef1f3c69d73ade035f8a73a7
anamaymb <anamay.belekar17@vit.edu>
2020-10-11 13:04:22 +0530

    TyringNN

--- C/TyringNN/TyringNN.c
+++ C/TyringNN/TyringNN.c
@@ -3,7 +3,7 @@
 #include<math.h>
 
 float weights[10][100],weights_prev[10][100];
-float output[10],expectedOp[10];
+float output[10],expectedOp[10],ExNum[10]={16,20,16,22,21,23,15,24,23,20};
 float input[10][100]={{0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,       //1                                    2
                       0 , 0 , 0 , 0 , 1 , 1 , 0 , 0 , 0 , 0 ,       //2
                       0 , 0 , 0 , 1 , 0 , 0 , 1 , 0 , 0 , 0 ,       //3
@@ -15,7 +15,7 @@ float input[10][100]={{0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,       //1        
                       0 , 0 , 1 , 1 , 1 , 1 , 1 , 1 , 0 , 0 ,       //9
                       0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },      //10
 
-                     {0 , 0 , 0 , 0 , 1 , 1 , 0 , 0 , 0 , 0 ,       //1                                     1
+                     {0 , 0 , 0 , 0 , 1 , 1 , 0 , 0 , 0 , 0 ,       //1                                     2
                       0 , 0 , 0 , 0 , 1 , 1 , 0 , 0 , 0 , 0 ,       //2
                       0 , 0 , 0 , 0 , 1 , 1 , 0 , 0 , 0 , 0 ,       //3
                       0 , 0 , 0 , 0 , 1 , 1 , 0 , 0 , 0 , 0 ,       //4
@@ -119,9 +119,7 @@ float calculateOp(int j,float h,int nthWeight, int nthRow)
 {
     float op=0;
     for (int k=0;k<100;k++)
-    {
         op=op+input[nthRow][k]*(weights[j][k]+h*(k==nthWeight && j==nthRow));
-    }
     
     return op;
 }
@@ -130,10 +128,8 @@ float calculateOp(int j,float h,int nthWeight, int nthRow)
 float sumOfSquares(int nthWeight, float offset, int nthRow)
 {
     float sum=0;
-    for(int i=0;i<10;i++)
-    {
+    for(int i=nthRow;i<=nthRow;i++)
         sum=sum+(expectedOp[i]-calculateOp(i,offset,nthWeight,nthRow))*(expectedOp[i]-calculateOp(i,offset,nthWeight,nthRow));
-    }
     return sum;
 }
 
@@ -142,9 +138,7 @@ float calculateOpPrev(int j,float h,int nthWeight,int nthRow)
 {
     float op=0;
     for (int k=0;k<100;k++)
-    {
         op=op+input[nthRow][k]*(weights_prev[j][k]+h*(k==nthWeight && j==nthRow));
-    }
 
     return op;
 }
@@ -153,10 +147,8 @@ float calculateOpPrev(int j,float h,int nthWeight,int nthRow)
 float sumOfPrevSquares(int nthWeight, float h, int nthRow)
 {
     float sum=0;
-    for(int i=0;i<10;i++)
-    {
+    for(int i=nthRow;i<=nthRow;i++)
         sum=sum+(expectedOp[i]-calculateOpPrev(i,h,nthWeight,nthRow))*(expectedOp[i]-calculateOpPrev(i,h,nthWeight,nthRow));
-    }
     return sum;
 }
 
@@ -169,17 +161,14 @@ for(int number=0; number<9;number++)
 {
     for(int i=0;i<10;i++)
     {
-        output[i]=0;expectedOp[i]=0;
+        expectedOp[i]=0;
         if(i==number)
-            expectedOp[i]=11;
-        
-            
+            expectedOp[i]=ExNum[i];
     }
     for(int j=0;j<100;j++)
     {
         weights[number][j]=0;weights_prev[number][j]=0;
     }
-
     // for(int j=0;j<10;j++)
     // {
     //     for (int i=0;i<100;i++)
@@ -196,46 +185,44 @@ for(int number=0; number<9;number++)
     //     printf("expectedOp[%d] : %f \n",i,expectedOp[i]);
     // }
     
-    float kp=0.002, h=0.01;
-    for (int count=0;count<80;count++)
+    float kp[10]={0.005 , 0.004 , 0.004 , 0.0035 , 0.003 , 0.0022 ,0.0052 ,0.002 ,0.002 ,0.004}, h=0.01;
+            //      2       1       3       4       5       6       7       8       9       0
+
+//          {       16,     20,     16,     22,     21,     23,     15,     24,     23,     20};
+
+    for (int count=0;count<100;count++)
     {
-            for(int i=0;i<100;i++)
-            {
-
-                if(sumOfPrevSquares(i,0,number)>sumOfPrevSquares(i,h,number))
-                {
-                    weights[number][i]=weights[number][i]+kp*abs(sumOfPrevSquares(0,0,number));
-                }
-                else if(sumOfPrevSquares(i,0,number)>sumOfPrevSquares(i,-h,number))
-                {
-                    weights[number][i]=weights[number][i]-kp*abs(sumOfPrevSquares(0,0,number));
-                }
-                
-
-            }
-        
-        // printf("\n");
-        
-            for(int i=0;i<100;i++)
-                weights_prev[number][i]=weights[number][i];
-        
         // printf("%f \n",sumOfSquares(0,0,number));
+        for(int i=0;i<100;i++)
+        {
+            if(sumOfPrevSquares(i,0,number)>sumOfPrevSquares(i,h,number))
+            {
+                weights[number][i]=weights[number][i]+kp[number]*abs(sumOfPrevSquares(0,0,number));
+            }
+            else if(sumOfPrevSquares(i,0,number)>sumOfPrevSquares(i,-h,number))
+            {
+                weights[number][i]=weights[number][i]-kp[number]*abs(sumOfPrevSquares(0,0,number));
+            }
+        }
+
+        for(int i=0;i<100;i++)
+            weights_prev[number][i]=weights[number][i];
+
     }
 
 
     for(int i=0;i<100;i++)
     {
         if(i%10==0)
-        printf("\n\n");
+            printf("\n\n");
         
         if(weights[number][i]!=0)
-        printf("%f ",weights[number][i]);
+            printf("%f ",weights[number][i]);
         else
-        printf("         ");
+            printf("         ");
     }
-printf("\n");
 
-
+    printf("\n");
 }
 
     return 0;
