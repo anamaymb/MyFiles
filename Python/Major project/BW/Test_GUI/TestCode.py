e7caa7951ea9d700d9eb855193d1b5756be96f6c
anamaymb <anamay.belekar17@vit.edu>
2020-11-16 19:11:04 +0530

    Major project- GUI cascade Trainer

--- Python/Major project/BW/Test_GUI/TestCode.py
+++ Python/Major project/BW/Test_GUI/TestCode.py
@@ -1,18 +1,18 @@
 import numpy as np
 import cv2
 
-shorten =2.1
+shorten =1.1
 
 face_cascade = cv2.CascadeClassifier('cascade.xml')
 
-img = cv2.imread('thermal_9.jpg')
+img = cv2.imread('D:/GITHUB/MyFiles-master/Python/Major project/thermal_14.jpg')
 
 img=cv2.resize(img,(int(img.shape[1]*shorten),int(img.shape[0]*shorten)))
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray,1.01,7)
 for (x,y,w,h) in faces:
-    img = cv2.rectangle(img,(x,y),(x+w,y+h),(250,250,12),2)
+    img = cv2.rectangle(img,(x,y),(x+int(1.4*w),y+int(1.4*h)),(0,0,255),2)
 
 cv2.imshow('img',img)
 cv2.waitKey(0)
